0c86ca914554cff8fe079a61eaea2c746f749b16
hayesp5 <hayesp5@my.erau.edu>
2026-04-05 21:29:18 -0700

    Add files via upload

---
+++ STATS.h
@@ -0,0 +1,13 @@
+///idk how this works but we're gonna try
+
+#ifndef STATS_H
+#define STATS_H
+typedef struct Character {
+	char name[50];
+	float LuckOfIllness; //chance out of 10 of surviving an illness
+	int dabloons;
+ 	int health;
+  int resources; // 
+} Character;
+
+#endif
